91bc8d331ecfd3b31f2bfc4398b284282984ee15
heitorcampos2 <heitormtb12@gmail.com>
2019-04-01 22:26:53 -0300

    v4 aula 01/04/2019

--- src/Conta.java
+++ src/Conta.java
@@ -5,7 +5,7 @@ public class Conta {
     String numero;
     String titular;
     String agencia;
-    boolean sacar;
+
     //cria metodo
         void depositar(double valor){
             this.saldo += valor; //this >> atributo da propria classe/ñ é obrigatorio colocar o this->apenas qnd o parametro tem msm nome do tributo
